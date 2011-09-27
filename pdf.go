791118c3d434410481b6db4a26436bb0f1eb4be6
Nathan Kerr <nathan@nathankerr.com>
2011-09-27 17:47:48 +0200

    now with template

--- pdf.go
+++ pdf.go
@@ -1,25 +1,65 @@
 package main
 
 import (
+	"bytes"
+	"exec"
 	"path/filepath"
 	"http"
-	"io"
+	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 	"url"
 )
 
+const PANDOC_DIR = "pandoc"
+
 func PDFServer(w http.ResponseWriter, req *http.Request) {
 	filename := draftFilename(req)
-	io.WriteString(w, filename)
+	iconv := exec.Command("iconv", "-f", "utf-16", "-t", "utf-8", filename)
+	// try automatic reencoding
+	// Writer for mac defaults to UTF-8 encoding, while
+	// Writer for iPad only uses UTF-16
+	//iconv := exec.Command("iconv", "-t", "utf-8", filename)
+	// I will make sure and force mac to UTF-16
 
-	
+	iconv_out, err := iconv.Output()
+	if err != nil {
+		log.Fatal("pdfserver iconv_out:", err)
+	}
 
-	//contents, err := ioutil.ReadFile(filename)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//w.Write(contents)
+	tmpdir, err := ioutil.TempDir("tmp/", "")
+	if err != nil {
+		log.Fatal("pdfserver tmpdir:", err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	template, err := filepath.Abs(PANDOC_DIR)
+	if err != nil {
+		log.Fatal("pdfserver template:", err)
+	}
+	template = filepath.Join(template, "template.tex")
+
+	pdf_out := filepath.Join(tmpdir, "output.pdf")
+	pandoc := exec.Command("markdown2pdf",
+		"-o", pdf_out,
+		"--xetex",
+		"--template", template,
+	)
+
+	pandoc.Stdin = bytes.NewBuffer(iconv_out)
+	pandoc.Stdout = os.Stdout
+	pandoc.Stderr = os.Stderr
+
+	if pandoc.Run() != nil {
+		log.Fatal("pdfserver pandoc.Run:", err)
+	}
+
+	content, err := ioutil.ReadFile(pdf_out)
+	if err != nil {
+		log.Fatal("pdfserver read content:", err)
+	}
+	w.Write(content)
 }
 
 func draftFilename(req *http.Request) string {
